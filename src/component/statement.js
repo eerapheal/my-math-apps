3589a658238a4bc04c1bc578cbe22e3f2bdfefe4
unknown <ekpenisiraphael@gmail.com>
2022-11-09 20:23:22 +0530

    Inner style applied

--- src/component/statement.js
+++ src/component/statement.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Statement = () => (
   <>
-    <p style={{ color: '#fff', lineHeight: 1, paddingTop: 80 }}>
+    <p style={{ color: '#ff0098', lineHeight: 1, padding: 120 }}>
       Mathematics is not about numbers, equations, computations or algorithms: it is
       about understanding. -William Paul Thurston
     </p>
